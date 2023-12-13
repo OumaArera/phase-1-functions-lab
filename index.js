e162a9cdae9dcbd1d57911799d9ae49b53fa3979
OumaArera <johnouma999@gmail.com>
2023-12-13 17:44:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 // Code your solution in this file!
-let x = 4;
-let y = 5
-let sum = x + y;
-console.log(sum)
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264; 
+}
+
+function distanceTravelledInFeet (start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(destination-start) * 264;
+    if (distance >= 2500){
+        return "cannot travel that far";
+
+    }else if (distance > 2000){
+        return 25;
+
+    }else if (distance > 400 && distance < 2000) {
+        return (distance - 400) * 0.02;
+    }else {
+        return 0;
+    }
+}
